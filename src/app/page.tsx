a09be6b7c1f69fd5dcd9f8ac4d7d1cdbe377e1fd
guivicentep <guivicente.gvp@hotmail.com>
2026-03-06 19:47:55 -0300

    feat: database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+'use client'
+
+import { useMutation, useQuery } from 'convex/react'
 import { Button } from '@/components/ui/button'
+import { api } from '../../convex/_generated/api'
 
 const Page = () => {
+  const projects = useQuery(api.projects.get)
+  const createProject = useMutation(api.projects.create)
+
   return (
-    <div>
-      <Button>Click</Button>
+    <div className="flex flex-col gap-2">
+      <Button
+        onClick={() =>
+          createProject({
+            name: 'New project12',
+          })
+        }
+      >
+        Add new
+      </Button>
+      {projects?.map((project) => (
+        <div className="border rounded p-2 flex flex-col" key={project._id}>
+          <p>{project.name}</p>
+          <p>Owner Id: {project.ownerId}</p>
+        </div>
+      ))}
     </div>
   )
 }
